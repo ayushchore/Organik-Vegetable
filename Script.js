d3166f5a2661973c2acb2669f831d3a8e1a741dd
ayushchore <ayushchore25@gmail.com>
2025-06-21 12:33:52 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,27 @@
+
+function blue1(){
+    document.getElementById("i5").style.display="block"
+}
+function red1(){
+    document.getElementById("i5").style.display="none"
+}
+function blue2(){
+    document.getElementById("i6").style.display="block"
+}
+function red2(){
+    document.getElementById("i6").style.display="none"
+}
+function blue3(){
+    document.getElementById("i7").style.display="block"
+}
+function red3(){
+    document.getElementById("i7").style.display="none"
+}
+function blue4(){
+    document.getElementById("i8").style.display="block"
+}
+function red4(){
+    document.getElementById("i8").style.display="none"
+}
+
+
